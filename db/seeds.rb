93d4ecacf31482b68e4f33ef5a33fdb696a7fe21
Alice Palmer Brown <palmerbrownalice@gmail.com>
2023-06-13 14:52:01 +0100

    mergeing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# users
+
+
 Book.create([{ title: "The Hobbit", author: "J.R.R. Tolkien", description: "The hobbit tells a fantastic pasta"}])
