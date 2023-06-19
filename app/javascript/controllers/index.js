a1c933ceb00ff841f84b2030612f7dbf1e71253b
Tristan Meillard <meillard.tristan@gmail.com>
2023-06-19 14:03:08 +0200

    navbar and chatroom

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,6 +7,9 @@ import { application } from "./application"
 import AddressAutocompleteController from "./address_autocomplete_controller"
 application.register("address-autocomplete", AddressAutocompleteController)
 
+import BooktempController from "./booktemp_controller"
+application.register("booktemp", BooktempController)
+
 import CheckboxController from "./checkbox_controller"
 application.register("checkbox", CheckboxController)
 
@@ -16,5 +19,5 @@ application.register("hello", HelloController)
 import MapController from "./map_controller"
 application.register("map", MapController)
 
-import BooktempController from "./booktemp_controller"
-application.register("booktemp", BooktempController)
+import StartChatController from "./start_chat_controller"
+application.register("start-chat", StartChatController)
