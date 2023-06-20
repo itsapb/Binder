c006a05a3233e6df8998175a6fe5257f709e4391
Alice Palmer Brown <palmerbrownalice@gmail.com>
2023-06-20 11:11:34 +0100

    icons & be inpspired page completed

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,4 +2,8 @@ class Chatroom < ApplicationRecord
   has_many :messages
   has_many :user_chatrooms
   has_many :users, through: :user_chatrooms
+
+  def other_user(current_user)
+    users.where.not(id: current_user.id).first
+  end
 end
