db922aae6c53cf7b40c47e368d3abd807122b3a9
enrico joland <enrico.joland@gmail.com>
2023-06-21 19:32:16 +0200

    quote page styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -33,27 +33,26 @@ class PagesController < ApplicationController
   end
 
   def inspire
-    @book_quotes =[
+    @book_quotes = [
       "'Not all those who wander are lost.'
       — J.R.R. Tolkien, The Fellowship of the Ring",
       "'Beware; for I am fearless, and therefore powerful.' — Mary Shelley, Frankenstein",
       "'I took a deep breath and listened to the old brag of my heart: I am, I am, I am.' — Sylvia Plath, The Bell Jar",
-      "'Love is or it ain’t. Thin love ain’t love at all.' — Toni Morrison, Beloved",
+      "'Love is or it ain't. Thin love ain't love at all.' — Toni Morrison, Beloved",
       "'Procrastination is the thief of time, collar him.' — Charles Dickens, David Copperfield",
       "'If you can concentrate always on the present, you'll be a happy man.'
-      – Paulo Coelho, The Alchemist",
-      "'Isn’t it nice to think that tomorrow is a new day with no mistakes in it yet?'
-      - L.M. Montgomery, Anne of Green Gables",
-      "'Today a reader, tomorrow a leader.' – Margaret Fuller",
-      "'A word after a word after a word is power.' – Margaret Atwood",
-      "'Books are a uniquely portable magic.' – Stephen King",
-      "Everything was beautiful, and nothing hurt. - Kurt Vonnegut, Slaughterhouse-Five",
-      "'There is always something left to love.' - Gabriel García Márquez, One Hundred Years of Solitude",
-      "'Anything worth dying for is certainly worth living for.' - Joseph Heller, Catch-22",
-      "'Time moves slowly, but passes quickly.' - Alice Walker, The Color Purple",
-      "'When I discover who I am, I'll be free.' - Ralph Ellison, Invisible Man",
+      — Paulo Coelho, The Alchemist",
+      "'Isn't it nice to think that tomorrow is a new day with no mistakes in it yet?'
+      — L.M. Montgomery, Anne of Green Gables",
+      "'Today a reader, tomorrow a leader.' — Margaret Fuller",
+      "'A word after a word after a word is power.' — Margaret Atwood",
+      "'Books are a uniquely portable magic.' — Stephen King",
+      "Everything was beautiful, and nothing hurt. — Kurt Vonnegut, Slaughterhouse-Five",
+      "'There is always something left to love.' — Gabriel García Márquez, One Hundred Years of Solitude",
+      "'Anything worth dying for is certainly worth living for.' — Joseph Heller, Catch-22",
+      "'Time moves slowly, but passes quickly.' — Alice Walker, The Color Purple",
+      "'When I discover who I am, I'll be free.' — Ralph Ellison, Invisible Man"
     ]
     @book_quote = @book_quotes.sample
   end
-
 end
